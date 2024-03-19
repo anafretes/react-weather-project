cda319d01267216b1cfe8ab6362d20cf33f27cff
Ana Fretes <anafretes@gmail.com>
2024-03-19 16:48:19 -0300

    Initiated setup

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
-function App() {
+export default function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
+      <h1>Weather App</h1>
+      <footer>
+        Built by Ana Fretes with{" "}
+        <a href="https://www.shecodes.io" target="_blank" rel="noreferrer">
+          SheCodes
         </a>
-      </header>
+        . This is{" "}
+        <a href="/" target="_blank" rel="noreferrer">
+          open-source
+        </a>{" "}
+        code.
+      </footer>
     </div>
   );
 }
-
-export default App;
