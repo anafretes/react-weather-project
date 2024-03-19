926bcd06ff2ea16adac56925ce1642c39d831407
Ana Fretes <anafretes@gmail.com>
2024-03-19 17:28:21 -0300

    Added bootstrap

--- src/App.js
+++ src/App.js
@@ -3,21 +3,23 @@ import "./App.css";
 export default function App() {
   return (
     <div className="App">
-      <h1>Weather App</h1>
-      <footer>
-        Built by Ana Fretes with{" "}
-        <a href="https://www.shecodes.io" target="_blank" rel="noreferrer">
-          SheCodes
-        </a>
-        . This is{" "}
-        <a
-          href="https://github.com/anafretes/react-weather-project"
-          target="_blank"
-          rel="noreferrer">
-          open-source
-        </a>{" "}
-        code.
-      </footer>
+      <div className="container">
+        <h1>Weather App</h1>
+        <footer>
+          Built by Ana Fretes with{" "}
+          <a href="https://www.shecodes.io" target="_blank" rel="noreferrer">
+            SheCodes
+          </a>
+          . This is{" "}
+          <a
+            href="https://github.com/anafretes/react-weather-project"
+            target="_blank"
+            rel="noreferrer">
+            open-source
+          </a>{" "}
+          code.
+        </footer>
+      </div>
     </div>
   );
 }
