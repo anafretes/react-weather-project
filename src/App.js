a7a58c4a7f6d4f4e60a22db4c1b941438488b94d
Ana Fretes <anafretes@gmail.com>
2024-03-19 18:25:10 -0300

    Added app skeleton

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
+import Weather from "./Weather";
 import "./App.css";
 
 export default function App() {
   return (
     <div className="App">
       <div className="container">
-        <h1>Weather App</h1>
+        <Weather />
         <footer>
           Built by Ana Fretes with{" "}
           <a href="https://www.shecodes.io" target="_blank" rel="noreferrer">
