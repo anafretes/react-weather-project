9906ce203056cf91dea7f4f50ebba9b9d3203122
Ana Fretes <anafretes@gmail.com>
2024-03-19 23:36:34 -0300

    Added skeleton style

--- src/Weather.js
+++ src/Weather.js
@@ -5,16 +5,21 @@ export default function Weather() {
   return (
     <div className="Weather">
       <form>
-        <div className="row gx-0">
+        <div className="row gx-1">
           <div className="col-9">
             <input
               type="search"
               placeholder="Enter a city"
               className="form-control"
+              autoFocus="on"
             />
           </div>
           <div className="col-3">
-            <input type="submit" value="Search" className="btn btn-primary" />
+            <input
+              type="submit"
+              value="Search"
+              className="btn btn-primary w-100"
+            />
           </div>
         </div>
       </form>
@@ -24,14 +29,16 @@ export default function Weather() {
         <li>Rainy</li>
       </ul>
       <div className="row">
-        <div className="col-6">
+        <div className="col-6 d-flex align-items-baseline pt-1">
           <img
-            src="https://ssl.gstatic.com/onebox/weather/64/rain_s_cloudy.png"
-            alt="Rainy and cloudy"
+            src="https://ssl.gstatic.com/onebox/weather/64/sunny.png"
+            alt="Sunny"
+            className="align-self-center"
           />
-          8°C
+          <span className="temperature">118</span>
+          <span className="unit">°C</span>
         </div>
-        <div className="col-6">
+        <div className="col-6 pt-4">
           <ul>
             <li>Precipitation: 15%</li>
             <li>Humidity: 72%</li>
