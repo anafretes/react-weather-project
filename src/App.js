5bd0b9bdf56f4d669afc84bc5f38fda9e84c803a
Ana Fretes <anafretes@gmail.com>
2024-03-20 17:35:55 -0300

    Added default property to component call

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ export default function App() {
   return (
     <div className="App">
       <div className="container">
-        <Weather />
+        <Weather defaultCity="New York" />
         <footer>
           Built by Ana Fretes with{" "}
           <a href="https://www.shecodes.io" target="_blank" rel="noreferrer">
